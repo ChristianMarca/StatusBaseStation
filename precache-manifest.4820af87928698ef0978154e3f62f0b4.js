a6be5d61b895fe22a8d83b08c43ec0950967ce48
ChristianMarca <cmarcag@hotmail.com>
2019-01-20 19:08:06 -0500

    Updates

---
+++ precache-manifest.4820af87928698ef0978154e3f62f0b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1dc33480b9f83532435",
+    "url": "/StatusBaseStation/static/css/main.dcd4d71b.chunk.css"
+  },
+  {
+    "revision": "e1dc33480b9f83532435",
+    "url": "/StatusBaseStation/static/js/main.e1dc3348.chunk.js"
+  },
+  {
+    "revision": "db847f076a3a8bb6e50a",
+    "url": "/StatusBaseStation/static/css/1.6b0fef73.chunk.css"
+  },
+  {
+    "revision": "db847f076a3a8bb6e50a",
+    "url": "/StatusBaseStation/static/js/1.db847f07.chunk.js"
+  },
+  {
+    "revision": "1a424e7ed3f991dc1ab9",
+    "url": "/StatusBaseStation/static/js/runtime~main.1a424e7e.js"
+  },
+  {
+    "revision": "27eb73986611a31bf5da1fe6a136c8e4",
+    "url": "/StatusBaseStation/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "%27//christianmarca.github.io/StatusBaseStation/precache-manifest.0bf5f2e41ad116b80f497f12cbd67d71.js"
+  "/StatusBaseStation/precache-manifest.4820af87928698ef0978154e3f62f0b4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("%27//christianmarca.github.io/StatusBaseStation/index.html", {
+workbox.routing.registerNavigationRoute("/StatusBaseStation/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
